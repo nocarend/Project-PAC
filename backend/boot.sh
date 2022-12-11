d573f6284e99746b7a7226ce096f7d04b4901267
Nikolay <58514394+nocarend@users.noreply.github.com>
2022-12-11 20:16:37 +0300

    Delete boot.sh

--- backend/boot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# shellcheck disable=SC2039
-source venv/bin/activate
-flask db init
-flask db migrate
-flask db stamp head
-flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - nsu:app
